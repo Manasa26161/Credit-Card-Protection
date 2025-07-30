91ed1130eee9bde8a02f1855909c8b36cd4933e4
Manasa26161 <manasamallini26@gmail.com>
2025-07-30 14:44:54 +0530

    Add files via upload

---
+++ Credit_card/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale-1.0">
+    </head>
+    <body>
+        <h1>This is php demo</h1>
+        <?php
+        echo "hello to everyone from php"
+        ?>
+    </body>
+</html>
